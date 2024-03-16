bab03bf226a4fcb90267761187d05fef5253e50e
ashu0x <ashu3324tosh@gmail.com>
2024-03-16 22:17:10 +0530

    final

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,11 +9,10 @@ const Hero = () => {
 
         <div className="flex pl-36 flex-col justify-center">
           <div className="font-semibold flex items-center uppercase">
-            <div className="w-10 h-[2px] mr-3 bg-cyan-700"></div>Hot Trend
+            <div className="w-10 h-[2px] mr-3 bg-cyan-700"></div>Products
           </div>
-          <h1 className="uppercase hover:scale-110 duration-500 text-gray-800 text-center text-[55px] md:text-[70px] leading-[1.1] font-semibold mb-4 ">Fresh Fashion Finds<br />
-            <span className=""><span className="text-gray-400">ne</span>w collection</span></h1>
-          <Link to={'/'} className='self-start uppercase font-semibold border-b-2 border-primary'>Discover More</Link>
+          <h1 className="uppercase hover:scale-110 duration-500 text-gray-800 text-center text-[55px] md:text-[70px] leading-[1.1] font-semibold mb-4 ">AI ECommerce Website<br />
+            <span className=""><span className="text-gray-400">ne</span>w </span></h1>
         </div>
       </div>
     </section>
